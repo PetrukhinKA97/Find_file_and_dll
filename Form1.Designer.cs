8f95b434873b8c61d3e16c3be027f1f7235b6aaf
PetrukhinKA97 <PetrukhinKA97@yandex.ru>
2022-03-19 21:40:39 +0300

    1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,135 @@ namespace Find_file_and_dll
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Пауза";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Pause_button);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(233, 426);
+            this.treeView1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(526, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 19);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Dll";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(260, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Click += new System.EventHandler(this.Path_find);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(260, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Время";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(392, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(117, 23);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Количество файлов";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(392, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Пуск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Find_buttonAsync);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(526, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(43, 19);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "File";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(526, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Сбросить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form_file_and_dll";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Path_find);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,90 @@ namespace Find_file_and_dll
 {
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Path_find(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = FBD.SelectedPath;
+            }
+        }
+
+        private async Task Find_buttonAsync(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Add(textBox1.Text);
+            treeView1.Nodes[0].Nodes.Add("Второй");
+            treeView1.Nodes[0].Nodes[0].Nodes.Add("3");
+            //вызываем рекурсивный метод
+            await Find(new DirectoryInfo(textBox1.Text));
+        }
+
+        private void Pause_button(object sender, EventArgs e)
+        {
+
+        }
+        // сохраняем
+        private void Save_treeView(object sender, EventArgs e)
+        {/*
+            //массив временый который и будем сериализовать
+            TreeNode[] tempNodes = new TreeNode[treeView1.Nodes.Count];
+            // заполняем его
+            for (int i = 0; i < treeView1.Nodes.Count; i++)
+                tempNodes[i] = treeView1.Nodes[i];
+            // сама сериализация
+            FileStream fs = new FileStream("TreeSave.xml", FileMode.Create);
+            SoapFormatter sf = new SoapFormatter();
+            sf.Serialize(fs, tempNodes);
+            fs.Close();
+            treeView1.Nodes.Clear(); // очищаем для наглядности*/
+        }
+        // а здесь загружаем
+        private void Load_treeView(object sender, EventArgs e)
+        {/*
+            TreeNode[] tempNodes;
+            FileStream fs = new FileStream("TreeSave.xml", FileMode.Open);
+            SoapFormatter sf = new SoapFormatter();
+            tempNodes = (TreeNode[])sf.Deserialize(fs);
+            treeView1.Nodes.AddRange(tempNodes);
+            fs.Close();*/
+        }
+
+        async static Task Find(DirectoryInfo root)
+        {
+            FileInfo[] File = null;
+            DirectoryInfo[] Dir = null;
+            // Получаем все файлы в текущем каталоге
+            try
+            {
+                File = root.GetFiles("*.*");
+            }
+            finally
+            {
+
+            }
+            if (File != null)
+            {
+                //выводим имена файлов в консоль
+                foreach (FileInfo fi in File)
+                {
+                    Console.WriteLine(fi.FullName);
+                }
+                //получаем все подкаталоги
+                Dir = root.GetDirectories();
+                //проходим по каждому подкаталогу
+                foreach (DirectoryInfo dirInfo in Dir)
+                {
+                    //РЕКУРСИЯ
+                    Find(dirInfo);
+                }
+            }
+        }
     }
 }
